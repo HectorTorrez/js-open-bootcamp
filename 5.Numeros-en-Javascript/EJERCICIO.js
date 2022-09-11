7eeeb85c068383c91173b27f9845549998f20be2
Héctor Buruca <torrezhectorb@gmail.com>
2022-09-10 19:34:11 -0600

    ejercicio

---
+++ 5.Numeros-en-Javascript/EJERCICIO.js
@@ -0,0 +1,14 @@
+let alturacentimetros = 165;
+
+
+let alturametros = 1.65;
+
+
+let peso = 67.5853;
+
+console.log(parseInt(alturametros));
+console.log(parseInt(peso));
+
+let maximo = Number.MAX_VALUE;
+
+console.log(maximo + 1);
